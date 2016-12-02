b99769916648cfc1738dbd877abf0f2331b1ea86
mina-park <smile_coco@naver.com>
2016-12-02 14:51:53 +0900

    Update 01_naver_login_and_search.py

--- selenium/01_naver_login_and_search.py
+++ selenium/01_naver_login_and_search.py
@@ -11,7 +11,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 #driver = webdriver.Firefox()
 driver = webdriver.Chrome()
-#driver = webdriver.Ie()
 
 #driver.maximize_window()
 driver.set_window_size(1000, 800)
