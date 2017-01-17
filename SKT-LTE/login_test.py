7a4c66e5ce68d93f1d5b85b1bdde3d601fb1336f
mina-park <smile_coco@naver.com>
2017-01-17 19:29:08 +0900

    DNS

--- SKT-LTE/login_test.py
+++ SKT-LTE/login_test.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import dns.resolver
 from selenium import webdriver
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -12,28 +13,47 @@ from selenium.webdriver.support import expected_conditions as EC
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-for i in range(1, 4):
+f = open("./log/SKT_Login_Test_{}.txt".format(time.ctime()), "w")
+
+for i in range(1, 3):
     driver = webdriver.Chrome()
     driver.maximize_window()
 
+    # 로그인 페이지 열기
     driver.get("https://www.jobplanet.co.kr/users/sign_in")
 
-    user_email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'user_email')))
+    page_loading_wait = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'user_email')))
 
-    user_email_login_id = driver.find_element_by_id("user_email")
-    user_email_login_id.clear()
-    user_email_login_id.send_keys("test053@intween.net")
-    user_email_login_pw = driver.find_element_by_id("user_password")
-    user_email_login_pw.clear()
-    user_email_login_pw.send_keys("qa123123")
-    driver.find_element_by_name("commit").click()
+    try:
+        # 로그인
+        user_email_login_id = driver.find_element_by_id("user_email")
+        user_email_login_id.clear()
+        user_email_login_id.send_keys("test053@intween.net")
+        user_email_login_pw = driver.find_element_by_id("user_password")
+        user_email_login_pw.clear()
+        user_email_login_pw.send_keys("qa123123")
+        driver.find_element_by_name("commit").click()
 
-    assert "함께 만드는 프리미엄 기업정보 | 잡플래닛" in driver.title
+        assert "함께 만드는 프리미엄 기업정보 | 잡플래닛" in driver.title
 
+        # 연결된 DNS 주소를 파일에 출력
+        myResolver = dns.resolver.Resolver()
+        fdata = "%s : %s \n" %(time.ctime(), myResolver.nameservers)
+        f.write(fdata)
+
+    except:
+        myResolver = dns.resolver.Resolver()
+        fdata = "%s : %s, 로그인 실패 \n" % (time.ctime(), myResolver.nameservers)
+        f.write(fdata)
+        continue
+
+    # 로그아웃
     driver.find_element_by_link_text("마이페이지").click()
     driver.find_element_by_link_text("로그아웃").click()
 
     time.sleep(10)
 
     driver.quit()
+
+f.close()
 
